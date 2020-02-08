6cfe088b67b0b1a7846d221cdefd3c4a358b7443
leti123 <diazcasatilloleticia@gmail.com>
2020-02-07 19:42:04 -0600

    menu principal lety

--- src/app/main-component/menu-principal/menu-principal.component.ts
+++ src/app/main-component/menu-principal/menu-principal.component.ts
@@ -2,14 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 
+// Confirmacion
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {ConfirmationService, MessageService} from 'primeng/api';
+
 @Component({
   selector: 'app-menu-principal',
   templateUrl: './menu-principal.component.html',
-  styleUrls: ['./menu-principal.component.css']
+  styleUrls: ['./menu-principal.component.css'],
+  providers:[ MessageService, ConfirmationService]
 })
 export class MenuPrincipalComponent implements OnInit {
 
-  constructor(private router: Router) {
+  constructor(private confirmationService: ConfirmationService, private router: Router) {
     let dataUser =  JSON.parse(sessionStorage.getItem('DataUser'));
     if(dataUser === null){
       this.router.navigate(['/']);
@@ -17,6 +22,20 @@ export class MenuPrincipalComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  salir(){
+    this.confirmationService.confirm({
+      message: '¿Deseas cerrar sesión?',
+      header: 'Confirmación',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        sessionStorage.removeItem('DataUser');
+        this.router.navigate(['/']);
+      },
+      reject: () => {
+        }
+    });
   }
 
 }

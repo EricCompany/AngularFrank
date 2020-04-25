cd48ea4409bf9d9fbe8f2cf0a817f9af0d0ef750
CabraNegra <davidcabrerag@outlook.com>
2020-04-24 23:38:48 -0500

    modulo3

--- src/app/Service/Modulo3/modulo3.service.ts
+++ src/app/Service/Modulo3/modulo3.service.ts
@@ -12,6 +12,8 @@ export class Modulo3Service {
   constructor(private endpoint: ConfigService, private http: HttpClient ) {}
   getDataTable(File: FormData): Observable<any> {
   return this.http.post(this.endpoint.getEndPoint() + 'Modulo2/Modulo2-Subir-Excel.php', File, );
-
   }
+  SubirExcel(File: FormData): Observable<any> {
+    return this.http.post(this.endpoint.getEndPoint() + 'Modulo3/Modulo3-Subir-Excel.php', File, );
+    }
 }
--- src/app/main-component/modulo3/modulo3.component.ts
+++ src/app/main-component/modulo3/modulo3.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
-import { MessageService } from 'primeng/api';
-
+import { MessageService, Message } from 'primeng/api';
+import { Modulo3Service } from '../../Service/Modulo3/modulo3.service';
+import { Verify } from '../../DTO/VerificacionExcelDTO';
 @Component({
   selector: 'app-modulo3',
   templateUrl: './modulo3.component.html',
@@ -21,10 +22,56 @@ import { MessageService } from 'primeng/api';
 })
 export class Modulo3Component implements OnInit {
   @BlockUI() blockUI: NgBlockUI;
+  cols: any;
+  fileToUpload: File = null;
+  fileToUploadName: string = null;
+  seleccionado: any;
+  mostrar: false;
+  verificacion: Verify;
 
-  constructor() { }
+  constructor(private MessageService: MessageService, private http: Modulo3Service) { }
 
   ngOnInit() {
+    this.cols = [
+      { field: 'no_control', header: 'No. DE CONTROL' },
+      { field: 'apellidoPaterno', header: 'Apellido Paterno' },
+      { field: 'apellidoMaterno', header: 'Apellido Matermo' },
+      { field: 'nombre', header: 'Numero de Seguro Social' }
+    ];
+  }
+  getTable(event) {
+    if (event.target.files[0]) {
+      this.fileToUpload = event.target.files[0];
+      this.fileToUploadName = event.target.files[0].name;
+    } else {
+      this.mostrar = false;
+    }
+  }
+
+  subirExcel() {
+    let formdata = new FormData();
+    formdata.append('excel', this.fileToUpload, this.fileToUploadName);
+    this.http.SubirExcel(formdata).subscribe(
+      (data) => {
+        this.verificacion = data;
+        if (data === 'El archivo ' + this.fileToUploadName + ' se cargo con EXITO.') {
+          this.MessageService.add({ key: 'excel', severity: 'success', summary: 'Estatus EXCEL', detail: "'" + this.verificacion + "'" });
+        } else {
+          this.MessageService.add({ key: 'excel', severity: 'error', summary: 'Estatus EXCEL', detail: "'" + this.verificacion + "'" });
+        }
+
+        this.blockUI.stop();
+        this.mostrar = false;
+        this.seleccionado = '';
+      },
+      (error) => {
+        this.MessageService.add({ key: 'excel', severity: 'info', summary: 'Estatus EXCEL', detail: 'Es posible que el nombre con el que intenta registrar el archivo este en uso, FAVOR DE VERIFICAR!' });
+        console.log(error);
+        this.blockUI.stop();
+        this.mostrar = false;
+        this.seleccionado = '';
+      }
+    );
   }
 
 }

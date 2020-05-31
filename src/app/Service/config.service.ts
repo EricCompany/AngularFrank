b8637da4907113f629e14716c788e2b654934fe8
CabraNegra <davidcabrerag@outlook.com>
2020-05-30 20:51:52 -0500

    modulo3 final2

--- src/app/Service/config.service.ts
+++ src/app/Service/config.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
- const  endpoint = 'http://localhost/ServiciosEscolares/';
- //const endpoint = 'http://40.124.53.212/ServiciosEscolares/';
+ //const  endpoint = 'http://localhost/ServiciosEscolares/';
+ const endpoint = 'http://40.124.53.212/ServiciosEscolares/';
 @Injectable({
   providedIn: 'root'
 })
